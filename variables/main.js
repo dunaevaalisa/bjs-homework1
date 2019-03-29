25031052018030b3847156aa551cdde63fbb64da
dunaevaalisa <48627604+dunaevaalisa@users.noreply.github.com>
2019-03-29 18:55:47 +0300

    Update main.js

---
+++ variables/main.js
@@ -0,0 +1,15 @@
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
+
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+let mark = (algebra + geography + physics)/3;
+console.log(mark);
+
+let name = Alisa;
+let message = «Привет, мир! Меня зовут »;
+console.log(${message} name);
